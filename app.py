26745adbd11a9f9e1236f5700d4b950ba89270e1
camilobmoreira <camilo.moreira@fatec.sp.gov.br>
2018-03-16 19:49:12 -0300

    Finalizando raspagem basica da pagina

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import re
 
 #Parametros para busca no site
 param_obs = 'Pronto em m'
-param_lance_inicial = 99999999
+param_valor_max = 99999999
 
 
 # specify the url
@@ -20,30 +20,36 @@ page = urllib2.urlopen(quote_page)
 soup = BeautifulSoup(page, 'html.parser')
 
 # Take out the <div> of name and get its value
-table_box = soup.find('table', attrs={'width':'1000', 'border':'0', 'cellpadding':'0', 'cellspacing':'0'})
+#table_box = soup.find_all('table', attrs={'width':'1000', 'border':'0'})
+table_box = soup.find_all('tr', attrs={'bgcolor':'#F3F4F5'})
 
-for tr in table_box.find('tr'):
+for tr in table_box:
     #Pega coluna da tabela onde possui as informações
     td = tr.find('td', attrs={'width':'100%', 'align': 'left'})
 
     #Pega observação do veiculo
     obs = td.find('font', attrs={'color':'#008080'}).find('b').text.strip()
+
+    #Verifica se a observacao esta de acordo com o parametro passado
     if param_obs != None and param_obs not in obs:
-        print('a')
         continue
 
     #Pega lance inicial do veiculo
     lance_inicial = int(td.find('font', attrs={'color':'#228B22'}).findNext().text.replace('R$ ', '').replace('.', '').replace(',', '').strip())
-    if param_lance_inicial != None and param_lance_inicial < lance_inicial:
-        print(lance_inicial)
+
+    #Pega maior lance
+    maior_lance = int(tr.find('font', attrs={'color':'#FF0000'}).text.replace('R$ ', '').replace('.', '').replace(',', '').strip())
+
+    #Verifica se o valor atual esta de acordo com o parametro passado
+    if param_valor_max is not None and (lance_inicial > param_valor_max or maior_lance > param_valor_max):
         continue    
 
+    #Pega a descricao (modelo/marca) do veiculo
     marca_modelo = td.find('b').find('font').text.strip()
-    ano = td.find('font', attrs={'color':'#505050'})
-    temp = td.find_all('font', attrs={'color':'#505050'})
+
+    # Pega o final da placa do carro
     placa = td.find(text=re.compile('Final')).strip()
-    temp = str(td)
-    ano = temp.split(str(ano))[1].split(str(placa))[0].split('|')[0].strip()
-    print(ano)
-    
-    break
+
+    #Pega o ano do carro
+    ano = td.find('font', attrs={'color':'#505050'})
+    ano = str(td).split(str(ano))[1].split(str(placa))[0].split('|')[0].strip()
